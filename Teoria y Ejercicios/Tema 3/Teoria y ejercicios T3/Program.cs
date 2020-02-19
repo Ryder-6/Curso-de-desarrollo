3862c3cf1c3f90788263f311cb7b3ff538aefb67
Ryder-6 <61141435+Ryder-6@users.noreply.github.com>
2020-02-19 13:12:51 +0100

    Update Program.cs

--- Teoria y Ejercicios/Tema 3/Teoria y ejercicios T3/Program.cs
+++ Teoria y Ejercicios/Tema 3/Teoria y ejercicios T3/Program.cs
@@ -7,6 +7,29 @@ namespace Teoria_y_ejercicios_T3
         static void Main(string[] args)
         {
             //Console.WriteLine("Hello World!");
+            ////////////Ejercicio 1/////////
+            //contador de cuantas repeticiones se esta hacendo en el bucle
+            //Console.Write("¿Cuantas veces repetimos?:");
+            //int i = int.Parse(Console.ReadLine());
+            //int x = 0;
+            //while (i != x - 1)
+            //{
+            //    Console.WriteLine("vamos por ella repeticion numero: " + i + " de " + x);
+            //    x ++;
+            //}
+
+            ////////////Ejercicio 2/////////
+            //Tabla de multiplicar
+            //Console.Write("¿Que tabla quieres?: ");
+            //int x = int.Parse(Console.ReadLine());
+            //Console.Write("Cuantas veces?: ");
+            //int y = int.Parse(Console.ReadLine());
+            //int i = 0;
+            //while (i++ != y)
+            //{
+            //    Console.WriteLine(x * i);
+            //}
+
 
         }
     }
