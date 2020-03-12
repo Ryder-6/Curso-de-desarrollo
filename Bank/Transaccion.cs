242548302ddbe6060b6a868648026baa14daddc6
Ryder-6 <61141435+Ryder-6@users.noreply.github.com>
2020-03-12 14:05:38 +0100

    Create Transaccion.cs

---
+++ Bank/Transaccion.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bank
+{
+    public struct Transaccion
+    {
+        public int cantidad;
+        public string concepto;
+
+        public Transaccion(int cantidad, string concepto)
+        {
+            this.cantidad = cantidad;
+            this.concepto = concepto;
+        }
+
+        public override string ToString()
+        {
+            string info = "";
+            info += "Cantidad: " + cantidad;
+            info += "\n";
+            info += "Concepto: " + concepto;
+            return info;
+        }
+    }
+}
