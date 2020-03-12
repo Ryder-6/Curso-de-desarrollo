cb02f3964fa9eeb7f4d58863b4d35a2fe3a06453
Ryder-6 <61141435+Ryder-6@users.noreply.github.com>
2020-03-12 14:36:15 +0100

    Create Enemy.cs

---
+++ Battle/Enemy.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Battle
+{
+    public class Enemy
+    {
+        public int vida;
+        public int maxAtk;
+        public int minAtk;
+
+        public Enemy(int vida, int maxAtk, int minAtk)
+        {
+            this.vida = vida;
+            this.maxAtk = maxAtk;
+            this.minAtk = minAtk;
+        }
+
+        public void Atacar(Enemy enemy)
+        {
+            Random random = new Random();
+            enemy.RecibirDaño(random.Next(minAtk, maxAtk + 1));
+        }
+
+        public void RecibirDaño(int daño)
+        {
+            vida -= daño;
+            if(vida <= 0)
+            {
+                Console.WriteLine("He muerto");
+            }
+        }
+    }
+}
