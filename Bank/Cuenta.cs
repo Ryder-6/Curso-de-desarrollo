4129494319ea2e138577f850d80f3eb75095739f
Ryder-6 <61141435+Ryder-6@users.noreply.github.com>
2020-03-12 14:05:35 +0100

    Create Cuenta.cs

---
+++ Bank/Cuenta.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bank
+{
+    public class Cuenta
+    {
+        public string nombrePropietario;
+        public List<Transaccion> transacciones;
+
+        public Cuenta(string nombrePropietario)
+        {
+            this.nombrePropietario = nombrePropietario;
+            transacciones = new List<Transaccion>();
+        }
+
+        public void AnadirTransaccion(Transaccion transaccion)
+        {
+            transacciones.Add(transaccion);
+        }
+
+        public void MostrarTransacciones()
+        {
+            Console.WriteLine("Transacciones: ");
+            for (int i = 0; i < transacciones.Count; i++)
+            {
+                Console.WriteLine("----------------");
+                Console.WriteLine(transacciones[i].ToString());
+                Console.WriteLine("----------------");
+            }
+        }
+    }
+}
