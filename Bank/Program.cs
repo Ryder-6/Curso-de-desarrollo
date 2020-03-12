13ecbfaedd694ddcfb3981c63815489672f32e73
Ryder-6 <61141435+Ryder-6@users.noreply.github.com>
2020-03-12 14:05:36 +0100

    Create Program.cs

---
+++ Bank/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Bank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Transaccion transaccion = new Transaccion(20, "Porque sí");
+            Cuenta cuenta = new Cuenta("Nacho");
+            cuenta.AnadirTransaccion(transaccion);
+            cuenta.AnadirTransaccion(transaccion);
+            cuenta.AnadirTransaccion(transaccion);
+            cuenta.MostrarTransacciones();
+        }
+    }
+}
